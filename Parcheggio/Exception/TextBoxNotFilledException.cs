d7a887e00c717417a42562728d91f7f8588c0010
Giacche18 <48328150+Giacche18@users.noreply.github.com>
2021-04-29 20:13:15 +0200

    Add files via upload

---
+++ Parcheggio/Exception/TextBoxNotFilledException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Parcheggio.Exception
+{
+    public class TextBoxNotFilledException : SystemException
+    {
+        public TextBoxNotFilledException() : base("Riempire tutti i campi prima di premere Enter!") { }
+    }
+}
---
+++ Parcheggio/Model/Veicolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcheggio.Model
+{
+    public class Veicolo
+    {
+        private string _tipoVeicolo;
+        private string _targa;
+
+        public Veicolo(string tipoVeicolo, string targa)
+        {
+            _tipoVeicolo = tipoVeicolo;
+            _targa = targa;
+        }
+
+        public string TipoVeicolo { get => _tipoVeicolo; set => _tipoVeicolo = value; }
+        public string Targa { get => _targa; set => _targa = value; }
+
+        public override string ToString()
+        {
+            return $"{_tipoVeicolo};{_targa};";
+        }
+    }
+}
