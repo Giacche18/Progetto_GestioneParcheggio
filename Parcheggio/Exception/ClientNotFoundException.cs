e0031f73b2240f7a0e6b93be187382854fa1df86
Giacche18 <48328150+Giacche18@users.noreply.github.com>
2021-04-29 20:16:59 +0200

    Delete Parcheggio directory

--- Parcheggio/Exception/ClientNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Parcheggio.Exception
-{
-    public  class ClientNotFoundException : SystemException
-    {
-        public ClientNotFoundException() : base("Cliente non trovato!") { }
-    }
-}
--- Parcheggio/Exception/TextBoxNotFilledException.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Parcheggio.Exception
-{
-    public class TextBoxNotFilledException : SystemException
-    {
-        public TextBoxNotFilledException() : base("Riempire tutti i campi prima di premere Enter!") { }
-    }
-}
