241bcb149b5a0108316a53bae93181f3ff613b5a
Giacche18 <48328150+Giacche18@users.noreply.github.com>
2021-04-29 19:31:13 +0200

    Add files via upload
    caricamento directory con codice progetto modificato

---
+++ Parcheggio/Model/Tempo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcheggio.Model
+{
+    public class Tempo
+    {
+        private string _tempoIngresso; 
+
+        public Tempo(string tempoIngresso)
+        {
+            _tempoIngresso = tempoIngresso;
+        }
+
+        public string Tempo_ingresso { get => _tempoIngresso; set => _tempoIngresso = value; }
+
+        public override string ToString()
+        {
+            return $"{_tempoIngresso};";
+        }
+    }
+}
---
+++ Parcheggio/formUpdateClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Parcheggio.Controller;
+
+namespace Parcheggio
+{
+    public partial class formUpdateClient : Form
+    {
+        private ControllerCheckIn controllerUpdateClient;
+
+        public formUpdateClient(ControllerCheckIn controlleupdateclient)
+        {
+            this.controllerUpdateClient = controlleupdateclient;
+
+            InitializeComponent();
+        }
+   
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            controllerUpdateClient.UpdateClient(listClient);
+            toolStripButton2.Enabled = false;
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
