430d0db8a4274bcde2b8c7befd7e93c1f0d14125
Giacche18 <48328150+Giacche18@users.noreply.github.com>
2021-04-29 18:43:06 +0200

    Add files via upload
    caricamento file per esame

---
+++ Parcheggio/Model/Anagrafica.cs
@@ -0,0 +1,41 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcheggio.Model
+{
+    public class Anagrafica
+    {
+        private string _nome;
+        private string _cognome;
+        private string _documento;
+        private string _numDocumento;
+        private Veicolo _veicolo;
+        private Tempo _tempo;
+
+        public Anagrafica(string nome, string cognome, string documento, string num_documento, Veicolo veicolo, Tempo tempo)
+        {
+            _nome = nome;
+            _cognome = cognome;
+            _documento = documento;
+            _numDocumento = num_documento;
+            _veicolo = veicolo;
+            _tempo = tempo;
+        }
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Cognome { get => _cognome; set => _cognome = value; }
+        public string Documento { get => _documento; set => _documento = value; }
+        public string Num_documento { get => _numDocumento; set => _numDocumento = value; }
+        internal Veicolo Veicolo { get => _veicolo; set => _veicolo = value; }
+        internal Tempo Tempo { get => _tempo; set => _tempo = value; }
+
+        public override string ToString()
+        {
+            return $"{_cognome};{_nome};{_documento};{_numDocumento};{_veicolo.ToString()}{_tempo.ToString()}";
+        }
+    }
+}
---
+++ Parcheggio/formCheckIn.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace Parcheggio
+{
+    partial class formCheckIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formCheckIn));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCognome = new System.Windows.Forms.TextBox();
+            this.textBoxNome = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxDocumento = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxTipoVeicolo = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTarga = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.oraIngresso = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxNumDocumento = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cognome";
+            // 
+            // textBoxCognome
+            // 
+            this.textBoxCognome.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCognome.Location = new System.Drawing.Point(199, 13);
+            this.textBoxCognome.Name = "textBoxCognome";
+            this.textBoxCognome.Size = new System.Drawing.Size(190, 29);
+            this.textBoxCognome.TabIndex = 1;
+            // 
+            // textBoxNome
+            // 
+            this.textBoxNome.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNome.Location = new System.Drawing.Point(199, 78);
+            this.textBoxNome.Name = "textBoxNome";
+            this.textBoxNome.Size = new System.Drawing.Size(190, 29);
+            this.textBoxNome.TabIndex = 2;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 423);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(48, 24);
+            this.toolStripButton1.Text = "Close";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(44, 24);
+            this.toolStripButton2.Text = "Enter";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Documento";
+            // 
+            // comboBoxDocumento
+            // 
+            this.comboBoxDocumento.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxDocumento.FormattingEnabled = true;
+            this.comboBoxDocumento.Items.AddRange(new object[] {
+            "Carta d\'identità",
+            "Patente",
+            "Passaporto"});
+            this.comboBoxDocumento.Location = new System.Drawing.Point(199, 144);
+            this.comboBoxDocumento.Name = "comboBoxDocumento";
+            this.comboBoxDocumento.Size = new System.Drawing.Size(121, 31);
+            this.comboBoxDocumento.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(180, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Veicolo";
+            // 
+            // comboBoxTipoVeicolo
+            // 
+            this.comboBoxTipoVeicolo.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTipoVeicolo.FormattingEnabled = true;
+            this.comboBoxTipoVeicolo.Items.AddRange(new object[] {
+            "Macchina",
+            "Camion",
+            "Scooter",
+            "Moto"});
+            this.comboBoxTipoVeicolo.Location = new System.Drawing.Point(198, 216);
+            this.comboBoxTipoVeicolo.Name = "comboBoxTipoVeicolo";
+            this.comboBoxTipoVeicolo.Size = new System.Drawing.Size(121, 31);
+            this.comboBoxTipoVeicolo.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(389, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Targa";
+            // 
+            // textBoxTarga
+            // 
+            this.textBoxTarga.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTarga.Location = new System.Drawing.Point(457, 218);
+            this.textBoxTarga.Name = "textBoxTarga";
+            this.textBoxTarga.Size = new System.Drawing.Size(190, 29);
+            this.textBoxTarga.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 296);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(180, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ora di Ingresso";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // oraIngresso
+            // 
+            this.oraIngresso.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.oraIngresso.Location = new System.Drawing.Point(199, 296);
+            this.oraIngresso.Name = "oraIngresso";
+            this.oraIngresso.Size = new System.Drawing.Size(180, 29);
+            this.oraIngresso.TabIndex = 13;
+            this.oraIngresso.Text = "12:00";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(389, 147);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 26);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Num.";
+            // 
+            // textBoxNumDocumento
+            // 
+            this.textBoxNumDocumento.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumDocumento.Location = new System.Drawing.Point(457, 144);
+            this.textBoxNumDocumento.Name = "textBoxNumDocumento";
+            this.textBoxNumDocumento.Size = new System.Drawing.Size(190, 29);
+            this.textBoxNumDocumento.TabIndex = 15;
+            // 
+            // formCheckIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNumDocumento);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.oraIngresso);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxTarga);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxTipoVeicolo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxDocumento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBoxNome);
+            this.Controls.Add(this.textBoxCognome);
+            this.Controls.Add(this.label1);
+            this.Name = "formCheckIn";
+            this.Text = "Check_In";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCognome;
+        private System.Windows.Forms.TextBox textBoxNome;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxDocumento;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxTipoVeicolo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTarga;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label oraIngresso;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxNumDocumento;
+    }
+}
